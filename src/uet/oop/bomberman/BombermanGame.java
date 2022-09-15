44b27ba952a635750915234dc7a3c01f6a6063e1
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2022-09-15 18:02:09 +0700

    Add a comment in render function

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -83,6 +83,7 @@ public class BombermanGame extends Application {
     }
 
     public void render() {
+        //hello
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         stillObjects.forEach(g -> g.render(gc));
         entities.forEach(g -> g.render(gc));
